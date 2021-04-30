4a30f4677758fa2278ae8a1b365b389319f49fee
Tomasz Śniechota <t.sniechota@gmail.com>
2021-04-30 19:31:25 +0200

    Skrypt wysyłający wiadomość mailową przez Gmail
    Skrypt wysyłający wiadomość mailowa na wskazany adres mailowy poprzez Gmaila w momencie gdy urządzenie o wskazanym adresie IP będzie online.

---
+++ SendMail.ps1
@@ -0,0 +1,17 @@
+﻿$from = "test@gmail.com"
+$to = "test@gmail.com"
+$subject = "Wiadomość testowa"
+$body = "Urządzenie o IP 192.168.0.11 jest już online"
+$stmpServer = "smtp.gmail.com"
+$smtpPort = "587"
+$utf8 = New-Object System.Text.UTF8Encoding
+$credential = Get-Credential
+
+do
+{
+    $ping = Test-Connection 192.168.0.11 -Quiet
+}
+until ($ping -eq $true)
+
+
+Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer $stmpServer -Port $smtpPort -UseSsl -Credential $credential -Encoding $utf8
